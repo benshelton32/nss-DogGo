828cdcfce8f5a0cf5719b7615f4d0904579d9270
Ben Shelton <bks-44@hotmail.com>
2022-09-22 18:13:14 -0500

    chapter 4 exercise 2

--- DogGo/Controllers/WalkersController.cs
+++ DogGo/Controllers/WalkersController.cs
@@ -1,4 +1,5 @@
 ﻿using DogGo.Models;
+using DogGo.Models.ViewModels;
 using DogGo.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +10,17 @@ namespace DogGo.Controllers
     public class WalkersController : Controller
     {
         private readonly IWalkerRepository _walkerRepo;
+        private readonly IWalkRepository _walkRepo;
+
+        public Walker Walker { get; private set; }
 
         // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
-        public WalkersController(IWalkerRepository walkerRepository)
+        public WalkersController(
+            IWalkerRepository walkerRepository, 
+            IWalkRepository walkRepository)
         {
             _walkerRepo = walkerRepository;
+            _walkRepo = walkRepository;
         }
 
         // GET: Walkers
@@ -28,13 +35,21 @@ namespace DogGo.Controllers
         public ActionResult Details(int id)
         {
             Walker walker = _walkerRepo.GetWalkerById(id);
+            List<Walk> walks = _walkRepo.GetWalksByWalkerId(id);
 
             if (walker == null)
             {
                 return NotFound();
             }
 
-            return View(walker);
+            WalkerProfileView wpv = new WalkerProfileView()
+            {
+                Walker = walker,
+                Walks = walks
+            };
+            
+
+            return View(wpv);
         }
 
         // GET: WalkersController/Create
